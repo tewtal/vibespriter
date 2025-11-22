c6f9c722da96e3e6c84a85a2d5d624cf40f0ca7f
Thomas Backmark <total@viskos.org>
2025-11-22 23:41:56 +0100

    Remove explicit public directory and copying configuration from Vite build.

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ export default defineConfig({
     base: '/vibespriter/',
     build: {
         outDir: 'dist',
-        assetsDir: 'assets',
-        // Ensure default folder is copied to dist
-        copyPublicDir: true
-    },
-    publicDir: 'default'
+        assetsDir: 'assets'
+    }
 })
